bccd5feed977e6d15c847df990a1f70734b193a9
Maikelb <maikel.bolderdijk@student.hu.nl>
2018-11-07 17:37:20 +0100

    String Quotations

---
+++ examples/Variable Types/Strings/Quotations.lua
@@ -0,0 +1,18 @@
+--
+-- File: Quotations.lua
+-- Author: Maikel Bolderdijk
+--
+
+-- [[ Double quote strings ]] --
+
+local stringDoubleQuote = "'this is a double qoute string'"
+print(stringDoubleQuote)
+
+-- [[ Single quote strings ]] --
+local stringSingleQuote = '"this is a single quote string"'
+print(stringSingleQuote)
+
+-- [[ Bracket string ]] --
+local stringBrackets = [["This is a bracket string with double quotes"]]
+local stringBrack = [['This is a bracket string wi'
+print(stringBrackets)
